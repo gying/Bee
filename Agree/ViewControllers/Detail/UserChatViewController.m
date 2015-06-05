8326356052312d4fa628158becc9e1f62f0e96cf
gying <gying@me.com>
2015-06-05 09:20:29 +0800

    fix warning

--- Agree/ViewControllers/Detail/UserChatViewController.m
+++ Agree/ViewControllers/Detail/UserChatViewController.m
@@ -452,8 +452,8 @@
         CGPoint location = [recognizer locationInView:self.userChatTableView];
         NSIndexPath * indexPath = [self.userChatTableView indexPathForRowAtPoint:location];
         //        UserChatTableViewCell *cell = (UserChatTableViewCell *)recognizer.view;
-        UserChatTableViewCell *cell = (UserChatTableViewCell *)[self.userChatTableView cellForRowAtIndexPath:indexPath];
-        [cell becomeFirstResponder];
+        UserChatTableViewCell *tableCell = (UserChatTableViewCell *)[self.userChatTableView cellForRowAtIndexPath:indexPath];
+        [tableCell becomeFirstResponder];
         
         UIMenuItem *itCopy = [[UIMenuItem alloc] initWithTitle:@"复制" action:@selector(handleCopyCell:)];
         UIMenuItem *itReSend = [[UIMenuItem alloc] initWithTitle:@"再次发送" action:@selector(handleResendCell:)];
